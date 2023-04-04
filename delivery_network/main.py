dd0f5cae988d32729b6bf62b1642765c90fe5ef5
thomasroussaux <126250801+thomasroussaux@users.noreply.github.com>
2023-04-05 00:39:28 +0200

    Update main.py

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,8 +1,32 @@
-from graph import Graph, graph_from_file
+import sys
+sys.setrecursionlimit(500000)
 
-
+from graph import Graph, graph_from_file, kruskal
+from UnionFind import UnionFind
 data_path = "input/"
-file_name = "network.01.in"
+file_name = "network.10.in"
+g = graph_from_file(data_path+file_name)
+print(g.min_power(1008,71000))
+ 
 
-g = graph_from_file(data_path + file_name)
-print(g)
+
+from graph import Graph, graph_from_file
+from time import perf_counter
+
+def tps_calcul(x):
+    tstart=perf_counter()
+    route="routes."+str(x)+".in"
+    network="network."+str(x)+".in"
+    g=graph_from_file(data_path+ network)
+    with open(data_path+route, "r") as file:
+        n=int(file.readline())
+        for k in range(10):
+            trajet=file.readline().split()
+            src,dest=int(trajet[0]),int(trajet[1])
+            g.min_power(src,dest)
+        tend=perf_counter()
+    return((tend-tstart)*n)/10
+
+#print(tps_calcul(4))
+
+   
